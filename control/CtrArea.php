1d75ab183d93ca8a29303c9f9892f0657c8cc14a
andrea arboleda tobon <andrea0026@hotmail.com>
2018-10-07 19:41:07 -0500

    Se agrega el crud Area, proyecto base

---
+++ control/CtrArea.php
@@ -0,0 +1,166 @@
+<?
+    class CtrArea{
+	var $objArea;
+	var $recordSet;
+        function CtrArea($objArea){
+
+        $this-> objArea=$objArea;
+        }
+
+
+        function guardar(){
+
+        $idarea=$this->objArea->getIdarea();
+    	 $nombre=$this->objArea->getNombre();
+        $fkarea=$this->objArea->getFkarea();
+
+       
+
+        $bd="repo";
+        $ObjConexion=new CtrConexion();
+        $enlace=$ObjConexion->conectar('localhost',$bd,'root','');
+
+
+        $consulta="insert into area values(".$idarea.",'".$nombre."',".$fkarea.")";
+         echo " Comando SQL : ". $consulta;
+		$recordSet=$ObjConexion->ejecutarSql($bd,$consulta);
+         $ObjConexion->cerrar($enlace);
+
+		if (!$recordSet)
+		{
+			die(" ERROR CON EL COMANDO SQL: ".mysql_error());
+		}
+		else
+		{
+		
+	$this->recordSet=$recordSet;
+
+		}
+
+        }
+
+
+        function consultar(){
+
+        $idarea=$this->objArea->getIdarea();
+        $bd="repo";
+        $ObjConexion=new CtrConexion();
+        $enlace=$ObjConexion->conectar('localhost',$bd,'root','');
+
+        $consulta="select * from area where idArea =".$idarea;
+       
+		    $recordSet=$ObjConexion->ejecutarSql($bd,$consulta);
+
+  
+        $registro = mysql_fetch_array($recordSet);
+
+
+        $this->objArea->setNombre($registro['nombre']);
+
+
+        $this->objArea->setFkarea($registro['fkarea']);
+
+        $ObjConexion->cerrar($enlace);
+		
+		if (!$recordSet)
+		{
+			die(" ERROR CON EL COMANDO SQL: ".mysql_error())."<br>";
+		}
+		else
+		{
+		
+	    return $this->objArea;
+
+		}
+
+        }
+
+ function borrar(){
+
+        $idarea=$this->objArea->getIdarea();
+        $bd="repo";
+        $ObjConexion=new CtrConexion();
+        $enlace=$ObjConexion->conectar('localhost',$bd,'root','');
+
+        $consulta="delete from area where idArea =".$idarea;
+       
+        $ObjConexion->ejecutarSql($bd,$consulta);
+
+        $ObjConexion->cerrar($enlace);
+    
+    if (!$recordSet)
+    {
+      die(" ERROR CON EL COMANDO SQL: ".mysql_error())."<br>";
+    }
+    else
+    {
+    
+      return $this->objArea;
+
+    }
+
+        }
+
+        
+
+
+        function listarAreas(){
+
+        $bd="repo";
+        $ObjConexion=new CtrConexion();
+        $enlace=$ObjConexion->conectar('localhost',$bd,'root','');
+        $consulta="select * from area";
+              $recordSet=$ObjConexion->ejecutarSql($bd,$consulta);
+
+
+              $numRegistros = mysql_num_rows($recordSet);
+
+              $mat[1][4]= $numRegistros;
+              $i=1;
+
+
+              while ($registro = mysql_fetch_array($recordSet)){
+
+              $mat[$i][1]=  $registro['idarea'];
+              $mat[$i][2]=  $registro['nombre'];
+              $mat[$i][3]=  $registro['fkarea'];
+              $i=$i+1;
+               }
+
+
+              mysql_free_result($recordSet);
+              $ObjConexion->cerrar($enlace);
+
+
+              return $mat;
+        }
+
+        function modificar(){
+
+           $idarea=$this->objArea->getIdarea();
+          $nombre=$this->objArea->getNombre();
+          $fkarea=$this->objArea->getFkarea();
+
+       
+
+        $bd="repo";
+        $ObjConexion=new CtrConexion();
+        $enlace=$ObjConexion->conectar('localhost',$bd,'root','');
+
+
+        $consulta="update area set nombre='".$nombre."', fkarea='".$fkarea."' where idarea =".$idarea;
+         
+
+         $recordSet=$ObjConexion->ejecutarSql($bd,$consulta);
+         $ObjConexion->cerrar($enlace);
+         if (!$recordSet){
+          die(" ERROR CON EL COMANDO SQL: ".mysql_error());
+        }
+
+
+    }
+
+    }
+
+
+?>
---
+++ vista/VistaArea.php
@@ -0,0 +1,110 @@
+<?
+
+include("../modelo/Area.php");
+include("../control/CtrArea.php");
+include("../control/CtrConexion.php");
+
+if($Guardar){
+
+    $idarea=$_POST["idarea"];
+    $nombre=$_POST["nombre"];
+    $fkarea= $_POST["fkarea"];
+
+
+    $ObjArea=new Area($idarea,$nombre,$fkarea);
+
+    $ObjCtrArea=new CtrArea($ObjArea);
+
+    $ObjCtrArea->guardar();
+    echo "<center>REGISTRO ALMACENADO</center>";
+}
+
+if($Consultar){
+		
+    $idarea=$_POST["idarea"];
+
+
+
+    $ObjArea=new Area($idarea,'','');
+
+    $ObjCtrArea=new CtrArea($ObjArea);
+     
+    $ObjCtrArea->consultar();
+
+    $nombre=$ObjArea->getNombre();
+    $fkarea= $ObjArea->getFkarea();
+
+  
+  echo "<center>
+<h3>Area</h3><form>
+	<table border=\"1\">
+		<tr>
+			<td>Codigo Area:</td>
+			<td><input type=\"text\" size=\"10\" maxlength=\"10\" name=\"idarea\"value='$idarea' ></td>
+		</tr>
+		<tr>
+			<td>Nombre Area:</td>
+			<td><input type=\"text\" size=\"60\" maxlength=\"60\" name=\"nombre\" value='$nombre'></td>
+		</tr>
+        		<tr>
+			<td>Código Jefe:</td>
+			<td><input type=\"text\" size=\"60\" maxlength=\"60\" name=\"fkarea\" value='$fkarea'></td>
+		</tr>
+	</table>
+	<input type=\"submit\" value=\"Guardar\" name=\"Guardar\">
+    <input type=\"submit\" value=\"Borrar\" name=\"Borrar\">
+    <input type=\"submit\" value=\"Modificar\" name=\"Modificar\">
+    <input type=\"submit\" value=\"Consultar\" name=\"Consultar\">
+    <input type=\"submit\" value=\"ListarAreas\" name=\"ListarAreas\">
+ </form></center>";
+ }
+
+if($Modificar){
+    
+    $idarea=$_POST["idarea"];
+    $nombre=$_POST["nombre"];
+    $fkarea= $_POST["fkarea"];
+
+    $ObjArea=new Area($idarea,$nombre,$fkarea);
+
+    $ObjCtrArea=new CtrArea($ObjArea);
+     
+    $ObjCtrArea->modificar();
+
+    $nombre=$ObjArea->getNombre();
+    $fkarea= $ObjArea->getFkarea();
+  }
+
+ if ($Borrar){
+    $idarea=$_POST["idarea"];
+    $ObjArea=new Area($idarea,'','');
+    $ObjCtrArea=new CtrArea($ObjArea);
+    $ObjCtrArea->borrar();
+ }
+
+if($ListarAreas){
+$objArea= new Area(0,'',0);
+$objCtrArea= new CtrArea($objArea);
+
+$mat= $objCtrArea->listarAreas();
+
+
+
+             $x="<table border='1'><tr><td>Codigo Area:</td><td>Nombre Area</td><td>fk area</td></tr>";
+              for($i=1;$i<=$mat[1][4];$i++){
+              		$x=$x."<tr><td>".$mat[$i][1]."</td><td>".$mat[$i][2]."</td><td>".$mat[$i][3].
+                  "</td><td><input type='submit' value='editar' name='Editar'/></td></tr>";
+              }
+              $x=$x."</table>";
+            
+              echo $x;
+}
+
+if($Editar){
+
+  echo '1';
+}
+
+
+
+?>
